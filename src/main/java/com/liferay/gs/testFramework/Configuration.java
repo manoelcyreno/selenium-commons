423441c7466e93ae31fd2fb6b2242c24d9670513
Manoel Cyreno <manoel.cyreno@liferay.com>
2017-08-31 16:03:16 -0300

    Update build gradle and the config file to be able to execute on Windows platform

--- src/main/java/com/liferay/gs/testFramework/Configuration.java
+++ src/main/java/com/liferay/gs/testFramework/Configuration.java
@@ -4,16 +4,22 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Configuration {
-	private static final String BUNDLE_NAME = "com.liferay.gs.testFramework.configuration"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME_MAC = "com.liferay.gs.testFramework.configuration"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME_WIN = "com.liferay.gs.testFramework.configurationWin"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+	private static final ResourceBundle RESOURCE_BUNDLE_MAC = ResourceBundle.getBundle(BUNDLE_NAME_MAC);
+	private static final ResourceBundle RESOURCE_BUNDLE_WIN = ResourceBundle.getBundle(BUNDLE_NAME_WIN);
 
 	private Configuration() {
 	}
 
 	public static String getString(String key) {
 		try {
-			return RESOURCE_BUNDLE.getString(key);
+			if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+				return RESOURCE_BUNDLE_WIN.getString(key);
+			} else {
+				return RESOURCE_BUNDLE_MAC.getString(key);
+			}
 		} catch (MissingResourceException e) {
 			return '!' + key + '!';
 		}
