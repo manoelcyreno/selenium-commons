678d2c7aac9d9cf4b465a72f8b9cd7a916ed078f
Manoel Cyreno <manoel.cyreno@liferay.com>
2017-10-26 10:43:37 -0300

    Apply Gradle Plugin

---
+++ src/main/java/com/liferay/gs/testFramework/seleniumCommonsPlugin.java
@@ -0,0 +1,9 @@
+package com.liferay.gs.testFramework;
+
+public class seleniumCommonsPlugin {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Liferay GS Selenium Commons Framework");
+	}
+
+}
